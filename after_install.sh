c39eb6fc467b4ddda7d6e03777a68e0c27f2c606
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-11 10:30:27 -0500

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -2,12 +2,16 @@
 
 cd /var/www
     chmod 644 openemr/library/sqlconf.php 
+    chmod 644 openemr/interface/globals.php
+    chmod 644 openemr/gacl/gacl.ini.php 
+    chmod 644 openemr/gacl/setup.class.php
     chmod 600 openemr/acl_setup.php 
     chmod 600 openemr/acl_upgrade.php 
-   # chmod 600 openemr/sl_convert.php
+    chmod 600 openemr/sl_convert.php
     chmod 600 openemr/setup.php 
     chmod 600 openemr/sql_upgrade.php 
     chmod 600 openemr/gacl/setup.php 
-    chmod 600 openemr/ippf_upgrade.php
+    
+    #chmod 600 openemr/ippf_upgrade.php
   
     
