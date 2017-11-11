83ec77e5c1edbf62096732d85753422e3b436956
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-11 10:43:37 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,6 +13,7 @@ else
    chown -R www-data:www-data /var/www/openemr/sites/default/era
    chown -R www-data:www-data /var/www/openemr/sites/default/documents
    chown -R www-data:www-data /var/www/openemr/sites/default/letter_templates
+   #this 3 maybe fail....
    chown -R www-data:www-data /var/www/openemr/library/freeb
    chown -R www-data:www-data /var/www/openemr/interface/main/calendar/modules/PostCalendar/pntemplates/compiled
    chown -R www-data:www-data /var/www/openemr/interface/main/calendar/modules/PostCalendar/pntemplates/cache
